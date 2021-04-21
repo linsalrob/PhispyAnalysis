08e0d2d45fd9019b74224fd1bd754734150f921c
Rob Edwards <raedwards@gmail.com>
2021-04-21 17:39:37 +0930

    Adding new functions to split text in columns

--- PhiSpyAnalysis/__init__.py
+++ PhiSpyAnalysis/__init__.py
@@ -1,7 +1,9 @@
 from .uncertainty import theils_u
 from .correct_dates import CantConvertDate, DateConverter
+from .correct_entries import file_to_accession, file_to_accession_name
 
 __all__ = [
     'theils_u',
-    'CantConvertDate', 'DateConverter'
+    'CantConvertDate', 'DateConverter',
+    'file_to_accession', 'file_to_accession_name'
 ]
---
+++ PhiSpyAnalysis/correct_entries.py
@@ -0,0 +1,24 @@
+"""
+Methods to correct text in entries
+"""
+
+import os
+import sys
+import re
+
+
+def file_to_accession_name(x):
+    regexp = re.compile('(\w+\.\d+)_([\w\.\-]+)_genomic.gbff.gz')
+    m = regexp.match(x)
+    if not m:
+        sys.stderr.write(f"WARNING: Regexp did not match {x}\n")
+        return (None, None)
+    return list(m.groups())
+
+def file_to_accession(x):
+    regexp = re.compile('(\w+\.\d+)_([\w\.\-]+)_genomic.gbff.gz')
+    m = regexp.match(x)
+    if not m:
+        sys.stderr.write(f"WARNING: Regexp did not match {x}\n")
+        return None
+    return m.groups()[0]
