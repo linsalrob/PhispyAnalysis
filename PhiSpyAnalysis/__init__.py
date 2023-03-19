628316355d13bd2739fc042e9683e68938b317ae
linsalrob <raedwards@gmail.com>
2023-03-19 15:33:19 +1030

    adding additional packages

--- PhiSpyAnalysis/__init__.py
+++ PhiSpyAnalysis/__init__.py
@@ -1,18 +1,20 @@
 from .uncertainty import theils_u
 from .correct_dates import CantConvertDate, DateConverter
-from .correct_entries import file_to_accession, file_to_accession_name, file_to_name
-from .read_data_frames import read_phages, read_gtdb, read_metadata, read_checkv, read_base_pp, read_gbk_metadata
+from .correct_entries import file_to_accession, file_to_accession_name, file_to_name, clean_decision
+from .read_data_frames import read_metadata
+from .read_data_frames import read_phages, read_gtdb, read_patric_metadata, read_checkv, read_base_pp, read_gbk_metadata
 from .read_data_frames import read_categories, read_transposons, read_transposons_per_phage, read_logo
 from .read_data_frames import read_insertion_lengths, read_phage_locations
+from .read_data_frames import bigtable, biglocations
 from .display import printmd
 from .genbank import is_gzip_file, parse_genbank
 
 __all__ = [
-    'theils_u',
-    'CantConvertDate', 'DateConverter',
-    'file_to_accession', 'file_to_accession_name', 'file_to_name',
-    'read_phages', 'read_gtdb', 'read_metadata', 'read_checkv', 'read_base_pp', 'read_gbk_metadata',
-    'read_categories', 'read_transposons_per_phage', 'read_logo', 'read_insertion_lengths', 'read_phage_locations',
-    'printmd', 'read_transposons', 'is_gzip_file', 'parse_genbank'
-
+    'CantConvertDate', 'DateConverter', 'biglocations', 'bigtable', 
+    'clean_decision', 'file_to_accession', 'file_to_accession_name', 
+    'file_to_name', 'is_gzip_file', 'parse_genbanktheils_u', 'printmd', 
+    'read_base_pp', 'read_categories', 'read_checkv', 'read_gbk_metadata',
+    'read_gtdb', 'read_insertion_lengths', 'read_logo', 'read_metadata',
+    'read_phage_locations', 'read_phages', 'read_transposons', 
+    'read_transposons_per_phage', 'theils_u'
 ]
